b61827b7bb7939879ae7da72e91972c2067ef276
Johannes Becker <jo.becker@gmail.com>
2011-02-20 16:05:14 +0100

    Metadata

--- banhammer.php
+++ banhammer.php
@@ -1,4 +1,23 @@
 <?php
+/*
+ Banhammer
+ Description: Blocks and SPAM comments based on a Google multiplicity lookup.
+ Author: Johannes Becker
+ Author URI: http://gibbonweb.net
+ Version: 0.1
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ */
+
 // the threshold value of the number of Google results
 // above which you consider the string to be SPAM.
 // Using a smaller value might increase false positives.
